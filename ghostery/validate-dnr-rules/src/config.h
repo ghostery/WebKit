5c4b19b46b7a16430e2c5f03f26c8701ba7fbd12
Krzysztof Modras <chrmod@chrmod.net>
2026-04-11 01:04:32 +0200

    Fix WTF include paths for CMake build

--- ghostery/validate-dnr-rules/src/config.h
+++ ghostery/validate-dnr-rules/src/config.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#ifdef HAVE_CONFIG_H
+#include "cmakeconfig.h"
+#endif
+
 #include <wtf/Platform.h>
 #include <wtf/PlatformEnable.h>
 #include <wtf/PlatformHave.h>
 #include <wtf/PlatformUse.h>
 
+#undef ENABLE_CONTENT_EXTENSIONS
 #define ENABLE_CONTENT_EXTENSIONS 1
+
+#ifndef WEBCORE_EXPORT
 #define WEBCORE_EXPORT
-#define WEBCORE_TESTSUPPORT_EXPORT
+#endif
